93d95c44103b96adc17a38df97ff628e46650356
evanhzg <evan.hoizey@ynov.com>
2024-09-10 18:55:49 +0200

    Re-structured Modals and UI, added insight for required fields

--- src/components/CompanyModal.tsx
+++ src/components/CompanyModal.tsx
@@ -13,7 +13,7 @@ import {
 	InputLabel,
 	SelectChangeEvent,
 } from '@mui/material';
-import { styled, BoxProps } from '@mui/system';
+import { Icon } from '@iconify/react';
 import '../styles/modal.css';
 
 interface Company {
@@ -37,11 +37,6 @@ interface CompanyModalProps {
 	onSave: (company: Company) => void | Promise<void>;
 	company?: Company;
 }
-
-const ScrollableBox = styled(Box)<BoxProps>(({ theme }) => ({
-	maxHeight: 'calc(100vh - 200px)',
-	overflowY: 'auto',
-}));
 
 const CompanyModal: React.FC<CompanyModalProps> = ({
 	open,
@@ -155,165 +150,256 @@ const CompanyModal: React.FC<CompanyModalProps> = ({
 					className='modal-title'>
 					{company ? "Modifier l'entreprise" : 'Ajouter une entreprise'}
 				</Typography>
-				<ScrollableBox
+				<Box
 					component='form'
 					className='modal-form'>
-					<TextField
-						className='modal-input'
-						fullWidth
-						margin='normal'
-						name='name'
-						label='Nom'
-						value={formData.name}
-						onChange={handleChange}
-						error={!!errors.name}
-						helperText={errors.name}
-					/>
-					<FormControl
-						fullWidth
-						margin='normal'
-						error={!!errors.size}>
-						<InputLabel>Taille</InputLabel>
-						<Select
-							name='size'
-							value={formData.size}
-							onChange={handleChange}>
-							<MenuItem value='1-10'>1-10</MenuItem>
-							<MenuItem value='11-50'>11-50</MenuItem>
-							<MenuItem value='51-100'>51-100</MenuItem>
-							<MenuItem value='101-500'>101-500</MenuItem>
-							<MenuItem value='501+'>501+</MenuItem>
-						</Select>
-						{errors.size && (
-							<Typography color='error'>{errors.size}</Typography>
-						)}
-					</FormControl>
-					<FormControl
-						fullWidth
-						margin='normal'
-						error={!!errors.type}>
-						<InputLabel>Type</InputLabel>
-						<Select
-							name='type'
-							value={formData.type}
-							onChange={handleChange}>
-							<MenuItem value='Startup'>Startup</MenuItem>
-							<MenuItem value='Agence'>Agence</MenuItem>
-							<MenuItem value='Grand Groupe'>Grand Groupe</MenuItem>
-							<MenuItem value='PME'>PME</MenuItem>
-							<MenuItem value='ESN'>ESN</MenuItem>
-							<MenuItem value='Fonction publique'>Fonction publique</MenuItem>
-							<MenuItem value='Autre'>Autre</MenuItem>
-						</Select>
-						{errors.type && (
-							<Typography color='error'>{errors.type}</Typography>
-						)}
-					</FormControl>
-					<FormControlLabel
-						control={
-							<Switch
-								checked={formData.acceptsUnsolicited}
-								onChange={(e) =>
-									setFormData((prev) => ({
-										...prev,
-										acceptsUnsolicited: e.target.checked,
-									}))
+					<div className='modal-form-group'>
+						<label>Informations générales</label>
+						<div>
+							<div className='form-field-container'>
+								<Icon
+									icon='fluent:important-12-filled'
+									className='required-icon'
+								/>
+								<TextField
+									className='modal-input'
+									fullWidth
+									margin='normal'
+									name='name'
+									label='Nom'
+									value={formData.name}
+									onChange={handleChange}
+									error={!!errors.name}
+									helperText={errors.name}
+								/>
+							</div>
+							<div className='form-field-container'>
+								<TextField
+									className='modal-input'
+									fullWidth
+									margin='normal'
+									name='logo'
+									label='URL du logo'
+									value={formData.logo}
+									onChange={handleChange}
+								/>
+							</div>
+						</div>
+					</div>
+
+					<div className='modal-form-group'>
+						<label>Détails de l'entreprise</label>
+						<div>
+							<div className='form-field-container'>
+								<Icon
+									icon='fluent:important-12-filled'
+									className='required-icon'
+								/>
+								<FormControl
+									fullWidth
+									margin='normal'
+									error={!!errors.size}>
+									<InputLabel>Taille</InputLabel>
+									<Select
+										name='size'
+										value={formData.size}
+										onChange={handleChange}>
+										<MenuItem value='1-10'>1-10</MenuItem>
+										<MenuItem value='11-50'>11-50</MenuItem>
+										<MenuItem value='51-100'>51-100</MenuItem>
+										<MenuItem value='101-500'>101-500</MenuItem>
+										<MenuItem value='501+'>501+</MenuItem>
+									</Select>
+									{errors.size && (
+										<Typography color='error'>{errors.size}</Typography>
+									)}
+								</FormControl>
+							</div>
+							<div className='form-field-container'>
+								<Icon
+									icon='fluent:important-12-filled'
+									className='required-icon'
+								/>
+								<FormControl
+									fullWidth
+									margin='normal'
+									error={!!errors.type}>
+									<InputLabel>Type</InputLabel>
+									<Select
+										name='type'
+										value={formData.type}
+										onChange={handleChange}>
+										<MenuItem value='Startup'>Startup</MenuItem>
+										<MenuItem value='Agence'>Agence</MenuItem>
+										<MenuItem value='Grand Groupe'>Grand Groupe</MenuItem>
+										<MenuItem value='PME'>PME</MenuItem>
+										<MenuItem value='ESN'>ESN</MenuItem>
+										<MenuItem value='Fonction publique'>
+											Fonction publique
+										</MenuItem>
+										<MenuItem value='Autre'>Autre</MenuItem>
+									</Select>
+									{errors.type && (
+										<Typography color='error'>{errors.type}</Typography>
+									)}
+								</FormControl>
+							</div>
+						</div>
+					</div>
+
+					<div className='modal-form-group'>
+						<label>Domaines et candidatures</label>
+						<div>
+							<div className='form-field-container'>
+								<TextField
+									className='modal-input'
+									fullWidth
+									margin='normal'
+									name='domains'
+									label='Domaines'
+									value={formData.domains.join(', ')}
+									onChange={handleArrayChange('domains')}
+									helperText='Séparez les domaines par des virgules'
+								/>
+							</div>
+							<div className='form-field-container'>
+								<FormControlLabel
+									control={
+										<Switch
+											checked={formData.acceptsUnsolicited}
+											onChange={(e) =>
+												setFormData((prev) => ({
+													...prev,
+													acceptsUnsolicited: e.target.checked,
+												}))
+											}
+											name='acceptsUnsolicited'
+										/>
+									}
+									label='Accepte les candidatures spontanées'
+								/>
+							</div>
+						</div>
+					</div>
+
+					<div className='modal-form-group'>
+						<label>Localisation</label>
+						<div>
+							<div className='form-field-container'>
+								<Icon
+									icon='fluent:important-12-filled'
+									className='required-icon'
+								/>
+								<TextField
+									className='modal-input'
+									fullWidth
+									margin='normal'
+									name='countries'
+									label='Pays'
+									value={formData.countries.join(', ')}
+									onChange={handleArrayChange('countries')}
+									error={!!errors.countries}
+									helperText={
+										errors.countries || 'Séparez les pays par des virgules'
+									}
+								/>
+							</div>
+							<div className='form-field-container'>
+								<Icon
+									icon='fluent:important-12-filled'
+									className='required-icon'
+								/>
+								<TextField
+									className='modal-input'
+									fullWidth
+									margin='normal'
+									name='cities'
+									label='Villes'
+									value={formData.cities.join(', ')}
+									onChange={handleArrayChange('cities')}
+									error={!!errors.cities}
+									helperText={
+										errors.cities || 'Séparez les villes par des virgules'
+									}
+								/>
+							</div>
+						</div>
+					</div>
+
+					<div className='modal-form-group'>
+						<label>Description</label>
+						<div>
+							<div className='form-field-container'>
+								<Icon
+									icon='fluent:important-12-filled'
+									className='required-icon'
+								/>
+								<TextField
+									className='modal-input'
+									fullWidth
+									margin='normal'
+									name='description'
+									label='Description complète'
+									value={formData.description}
+									onChange={handleChange}
+									multiline
+									rows={4}
+									error={!!errors.description}
+									helperText={errors.description}
+								/>
+							</div>
+						</div>
+						<div>
+							<div className='form-field-container'>
+								<Icon
+									icon='fluent:important-12-filled'
+									className='required-icon'
+								/>
+								<TextField
+									className='modal-input'
+									fullWidth
+									margin='normal'
+									name='shortDescription'
+									label='Description courte'
+									value={formData.shortDescription}
+									onChange={handleChange}
+									error={!!errors.shortDescription}
+									helperText={errors.shortDescription}
+								/>
+							</div>
+						</div>
+					</div>
+
+					<div className='modal-form-group'>
+						<label>Statut</label>
+						<div>
+							<FormControlLabel
+								control={
+									<Switch
+										checked={formData.available}
+										onChange={(e) =>
+											setFormData((prev) => ({
+												...prev,
+												available: e.target.checked,
+											}))
+										}
+										name='available'
+									/>
 								}
-								name='acceptsUnsolicited'
-							/>
-						}
-						label='Accepte les candidatures spontanées'
-					/>
-					<TextField
-						className='modal-input'
-						fullWidth
-						margin='normal'
-						name='domains'
-						label='Domaines'
-						value={formData.domains.join(', ')}
-						onChange={handleArrayChange('domains')}
-						helperText='Séparez les domaines par des virgules'
-					/>
-					<TextField
-						className='modal-input'
-						fullWidth
-						margin='normal'
-						name='countries'
-						label='Pays'
-						value={formData.countries.join(', ')}
-						onChange={handleArrayChange('countries')}
-						error={!!errors.countries}
-						helperText={errors.countries || 'Séparez les pays par des virgules'}
-					/>
-					<TextField
-						className='modal-input'
-						fullWidth
-						margin='normal'
-						name='cities'
-						label='Villes'
-						value={formData.cities.join(', ')}
-						onChange={handleArrayChange('cities')}
-						error={!!errors.cities}
-						helperText={errors.cities || 'Séparez les villes par des virgules'}
-					/>
-					<TextField
-						className='modal-input'
-						fullWidth
-						margin='normal'
-						name='description'
-						label='Description'
-						value={formData.description}
-						onChange={handleChange}
-						multiline
-						rows={4}
-						error={!!errors.description}
-						helperText={errors.description}
-					/>
-					<TextField
-						className='modal-input'
-						fullWidth
-						margin='normal'
-						name='shortDescription'
-						label='Description courte'
-						value={formData.shortDescription}
-						onChange={handleChange}
-						error={!!errors.shortDescription}
-						helperText={errors.shortDescription}
-					/>
-					<TextField
-						className='modal-input'
-						fullWidth
-						margin='normal'
-						name='logo'
-						label='URL du logo'
-						value={formData.logo}
-						onChange={handleChange}
-					/>
-					<FormControlLabel
-						control={
-							<Switch
-								checked={formData.available}
-								onChange={(e) =>
-									setFormData((prev) => ({
-										...prev,
-										available: e.target.checked,
-									}))
+								label={
+									<span
+										style={{
+											color: formData.available
+												? 'var(--success-color)'
+												: 'var(--error-color)',
+										}}>
+										{formData.available ? 'Disponible' : 'Indisponible'}
+									</span>
 								}
-								name='available'
 							/>
-						}
-						label={
-							<span
-								style={{
-									color: formData.available
-										? 'var(--success-color)'
-										: 'var(--error-color)',
-								}}>
-								{formData.available ? 'Disponible' : 'Indisponible'}
-							</span>
-						}
-					/>
-				</ScrollableBox>
+						</div>
+					</div>
+				</Box>
 				<Box className='modal-actions'>
 					<Button
 						variant='contained'
