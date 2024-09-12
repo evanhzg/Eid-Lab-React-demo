2452848b80f4d6e4455f93e8965569f5fbbccc2e
evanhzg <evan.hoizey@ynov.com>
2024-09-12 19:20:35 +0200

    reworked 404 page

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Typography, Box } from '@mui/material';
-import { Icon } from '@iconify/react';
+import { Box } from '@mui/material';
 import '../styles/error404.css';
 
 const Error404: React.FC = () => {
@@ -9,14 +8,20 @@ const Error404: React.FC = () => {
 
 	return (
 		<Box className='error-404-container'>
-			<Icon
-				icon='mdi:paw-off'
-				className='error-404-icon'
+			<img
+				src='/unpaws-logo.png'
+				alt='Unpaws Logo'
+				className='error-404-logo'
 			/>
 			<h2 className='error-404-title'>404: Page Not Found</h2>
 			<p className='error-404-subtitle'>
 				Oops! Looks like this page has gone for a walk.
 			</p>
+			<button
+				className='error-404-button'
+				onClick={() => navigate('/')}>
+				find a new home
+			</button>
 		</Box>
 	);
 };
